f9ed10b55681ced78a466d98ee2eead7c8ea268c
kendalldc <67480485+kendalldc@users.noreply.github.com>
2020-08-07 10:36:56 -0400

    Add files via upload

---
+++ src/Components/Channels.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+
+function Channels() {
+    return (
+      <div className="channels">
+         
+  <h2>Channels</h2>
+  <h4>TUNE IN TO THE NAUTILUS' DEEP DIVE PORTALS</h4>
+  
+      </div>
+  
+  
+  
+    );
+  }
+
+export default Channels
