8cb57e534bf4a91770f9d2e11b46474343987c48
kendalldc <67480485+kendalldc@users.noreply.github.com>
2020-08-07 10:38:04 -0400

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+
+function Footer() {
+    return (
+      <div className="main">
+         
+  
+  
+         <h1>Footer</h1>
+  
+      </div>
+  
+  
+  
+    );
+  }
+
+export default Footer
