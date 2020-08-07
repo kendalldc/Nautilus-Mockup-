820c5285edefe66ae4af1d0de5c92f4ceb63f25c
kendalldc <67480485+kendalldc@users.noreply.github.com>
2020-08-07 10:37:39 -0400

    Add files via upload

---
+++ src/Components/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.png';
+
+
+function Main() {
+    return (
+      <div className="main">
+<ul className="nav">
+<img src={logo}/>
+         
+         
+            <li>Issues</li>
+            <li>Topics</li>
+            <li>Blog</li>
+            <li>Newsletter</li>
+    
+          </ul>
+  
+  
+      </div>
+  
+  
+  
+    );
+  }
+
+
+export default Main
